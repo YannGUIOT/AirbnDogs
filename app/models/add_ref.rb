4c230f4205adffc7194a135353aaa7b2cc322d18
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-27 15:53:39 +0200

    AirBn'Dogs

---
+++ app/models/add_ref.rb
@@ -0,0 +1,2 @@
+class AddRef < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+Dog.destroy_all
+Dogsitter.destroy_all
+Stroll.destroy_all
+City.destroy_all
+
+10.times do 
+  city = City.create!(
+    name: Faker::Address.city
+  )
+end
+
+10.times do 
+  dog = Dog.create!(
+    name: Faker::Creature::Dog.name,
+    city: City.all.sample
+  )
+  dogsitter = Dogsitter.create!(
+    name: Faker::Name.first_name,
+    city: City.all.sample
+  )
+end
+
+15.times do 
+  dogsitter = Dogsitter.all.sample
+  dog = Dog.all.sample
+  stroll = Stroll.create!(
+    dogsitter:dogsitter, 
+    dog:dog, 
+    city: dog.city,
+    date: Faker::Time.between_dates(from: Date.today, to: Date.today + 10, period: :day))
+end
